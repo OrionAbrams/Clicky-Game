55043ddc6ea5b3b3ca484fd0eb2245a6db7a7130
Parashara <parashara@gmail.com>
2019-01-20 14:23:17 -0800

    testing react github pages with diff content

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import SpongeBobCard from "./components/SpongeBobCard";
+// import SquidwardCard from "./components/SquidwardCard";
+// import MrKrabsCard from "./components/MrKrabsCard";
+import FriendCard from "./components/FriendCard"
+import friends from "./friends.json"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Wrapper>
+      <Title>Friends List</Title>
+      {friends.map(friend => {
+        return (
+  <FriendCard 
+  name={friend.name}
+  image={friend.image}
+  occupation={friend.occupation}
+  location={friend.location}
+/>
+
+        )
+      })} 
+    </Wrapper>
+  );
 }
 
 export default App;
