58905c13c04ac11e11c27a778613d18b641c8e24
Parashara <parashara@gmail.com>
2019-01-23 12:56:43 -0800

    Updates

---
+++ precache-manifest.175e9b6469a9d51bb02e5b0e850c05f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92a93cce542673ed4b67",
+    "url": "/Clicky-Game/static/css/main.97e24d6b.chunk.css"
+  },
+  {
+    "revision": "92a93cce542673ed4b67",
+    "url": "/Clicky-Game/static/js/main.92a93cce.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "07cb6fcdb37eef1e36cd90eb06131019",
+    "url": "/Clicky-Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clicky-Game/precache-manifest.74f7f733b3c71fef777ac5a58088a452.js"
+  "/Clicky-Game/precache-manifest.175e9b6469a9d51bb02e5b0e850c05f8.js"
 );
 
 workbox.clientsClaim();
