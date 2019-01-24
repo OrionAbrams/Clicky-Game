3e5c5652451ee79a9841df3c3968e9c3c736d4a8
Parashara <parashara@gmail.com>
2019-01-23 20:44:25 -0800

    took out one slash on homepage to see if images load

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,38 +1,12 @@
 import React from "react";
 import "./style.css";
 
-// function addScore(){
-//   if (this.alreadyClicked){
-//     alert("You lose!")
-//   }
-//   else {
-//     this.alreadyClicked = true;
-//     // score++
-//   }
-// }
-
 function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
         <img onClick={() => props.clickedIt(props.id)} className="click-me" alt={props.name} src={props.image} />
       </div>
-      {/* <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div> */}
-      {/* <span  className="remove">
-        𝘅
-      </span> */}
     </div>
   );
 }
